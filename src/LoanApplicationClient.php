c3d70ab47691951645b20be710d4a161cd704639
Vlad Shashkov <shashkov.root@gmail.com>
2023-10-07 22:59:57 +0700

    Fixed code-style

--- src/LoanApplicationClient.php
+++ src/LoanApplicationClient.php
@@ -5,23 +5,20 @@ declare(strict_types=1);
 namespace Vanta\Integration\B2posSoapClient;
 
 use Vanta\Integration\B2posSoapClient\Client\LoanApplication\Request\Full\NewLoanApplicationRequest as NewLoanApplicationRequestFull;
-use Vanta\Integration\B2posSoapClient\Client\LoanApplication\Request\GetLoanApplicationStatusRequest;
 use Vanta\Integration\B2posSoapClient\Client\LoanApplication\Request\Short\NewLoanApplicationRequest as NewLoanApplicationRequestShort;
 use Vanta\Integration\B2posSoapClient\Client\LoanApplication\Response\GetLoanApplicationStatusResponse;
 
 interface LoanApplicationClient
 {
     /**
-     * @throws B2PosSoapClientException
-     *
      * @return numeric-string
+     * @throws B2PosSoapClientException
      */
     public function newLoanApplicationShort(NewLoanApplicationRequestShort $request): string;
 
     /**
-     * @throws B2PosSoapClientException
-     *
      * @return numeric-string
+     * @throws B2PosSoapClientException
      */
     public function newLoanApplicationFull(NewLoanApplicationRequestFull $request): string;
 
